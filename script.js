ae76bf638d6524f1a5e13225c7d0830837337fa6
Yelyzaveta Samodid <lizolda@YelyzavasLaptop.attlocal.net>
2024-04-04 19:17:57 -0500

    Challenge

---
+++ script.js
@@ -0,0 +1,26 @@
+function calculator(num1, num2, operator) {
+    let result;
+  
+    switch (operator) {
+      case '+':
+        result = num1 + num2;
+        break;
+      case '-':
+        result = num1 - num2;
+        break;
+      case '*':
+        result = num1 * num2;
+        break;
+      case '/':
+        result = num1 / num2;
+        break;
+      default:
+        result = 'Invalid Operator';
+    }
+  
+    console.log(result);
+    return result;
+  }
+  
+  calculator(5, 2, '&');
+  
